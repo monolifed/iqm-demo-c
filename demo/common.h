33521f8c7eb294c7d6b6413e4dba639993c1a0cd
monolifed <6624464+monolifed@users.noreply.github.com>
2020-03-29 23:27:09 +0300

    some changes & gitignore

--- demo/common.h
+++ demo/common.h
@@ -53,28 +53,29 @@ typedef float vec4[4];
 typedef vec4 quat;
 typedef vec4 mat3x4[3]; //row major
 
-//https://en.wikipedia.org/wiki/Quaternions_and_spatial_rotation#Quaternion-derived_rotation_matrix
+// https://en.wikipedia.org/wiki/Quaternions_and_spatial_rotation#Quaternion-derived_rotation_matrix
 static void mat3x4_from_rst(mat3x4 m, const quat r, const vec3 t, const vec3 s)
 {
-	float sx = 2 * s[0], sy = 2 * s[1], sz = 2 * s[2];
+	float sx = 2.f * s[0], sy = 2.f * s[1], sz = 2.f * s[2];
 	float x = r[0], y = r[1], z = r[2], w = r[3];
-	float n = sqrtf(x * x + y * y + z * z + w * w);
-	x /= n; y /= n; z /= n; w /= n;
-	m[0][0] = sx * (0.5 - (y * y + z * z));
+	float n = 1.f / sqrtf(x * x + y * y + z * z + w * w);
+	x *= n; y *= n; z *= n; w *= n;
+	m[0][0] = sx * (0.5f - (y * y + z * z));
 	m[0][1] = sy * (x * y - w * z);
 	m[0][2] = sz * (x * z + w * y);
 	m[0][3] = t[0];
 	m[1][0] = sx * (x * y + w * z);
-	m[1][1] = sy * (0.5 - (x * x + z * z));
+	m[1][1] = sy * (0.5f - (x * x + z * z));
 	m[1][2] = sz * (y * z - w * x);
 	m[1][3] = t[1];
 	m[2][0] = sx * (x * z - w * y);
 	m[2][1] = sy * (y * z + w * x);
-	m[2][2] = sz * (0.5 - (x * x + y * y));
+	m[2][2] = sz * (0.5f - (x * x + y * y));
 	m[2][3] = t[2];
 }
 
-static void vec4_matmul(vec4 a, mat3x4 m, vec4 b)
+// a = b * M
+static void vec4_matmul(vec4 a, vec4 b, mat3x4 m)
 {
 	a[0] = m[0][0] * b[0] + m[1][0] * b[1] + m[2][0] * b[2];
 	a[1] = m[0][1] * b[0] + m[1][1] * b[1] + m[2][1] * b[2];
@@ -82,26 +83,13 @@ static void vec4_matmul(vec4 a, mat3x4 m, vec4 b)
 	a[3] = m[0][3] * b[0] + m[1][3] * b[1] + m[2][3] * b[2] + b[3];
 }
 
+//ret =  f * g
 static void mat3x4_mul(mat3x4 ret, mat3x4 f, mat3x4 g)
 {
 	mat3x4 m;
-	vec4_matmul(m[0], g, f[0]);
-	vec4_matmul(m[1], g, f[1]);
-	vec4_matmul(m[2], g, f[2]);
-/*
-	m[0][0] = g[0][0] * f[0][0] + g[1][0] * f[0][1] + g[2][0] * f[0][2];
-	m[0][1] = g[0][1] * f[0][0] + g[1][1] * f[0][1] + g[2][1] * f[0][2];
-	m[0][2] = g[0][2] * f[0][0] + g[1][2] * f[0][1] + g[2][2] * f[0][2];
-	m[0][3] = g[0][3] * f[0][0] + g[1][3] * f[0][1] + g[2][3] * f[0][2] + f[0][3];
-	m[1][0] = g[0][0] * f[1][0] + g[1][0] * f[1][1] + g[2][0] * f[1][2];
-	m[1][1] = g[0][1] * f[1][0] + g[1][1] * f[1][1] + g[2][1] * f[1][2];
-	m[1][2] = g[0][2] * f[1][0] + g[1][2] * f[1][1] + g[2][2] * f[1][2];
-	m[1][3] = g[0][3] * f[1][0] + g[1][3] * f[1][1] + g[2][3] * f[1][2] + f[1][3];
-	m[2][0] = g[0][0] * f[2][0] + g[1][0] * f[2][1] + g[2][0] * f[2][2];
-	m[2][1] = g[0][1] * f[2][0] + g[1][1] * f[2][1] + g[2][1] * f[2][2];
-	m[2][2] = g[0][2] * f[2][0] + g[1][2] * f[2][1] + g[2][2] * f[2][2];
-	m[2][3] = g[0][3] * f[2][0] + g[1][3] * f[2][1] + g[2][3] * f[2][2] + f[2][3];
-*/
+	vec4_matmul(m[0], f[0], g);
+	vec4_matmul(m[1], f[1], g);
+	vec4_matmul(m[2], f[2], g);
 	memcpy(ret, m, sizeof(mat3x4));
 }
 
@@ -110,16 +98,16 @@ static void mat3x4_invert(mat3x4 i, mat3x4 m)
 	float n, x, y, z;
 	
 	x = m[0][0]; y = m[1][0]; z = m[2][0];
-	n = sqrtf(x * x + y * y + z * z);
-	i[0][0] = x / n; i[0][1] = y / n; i[0][2] = z / n;
+	n = 1.f / (x * x + y * y + z * z);
+	i[0][0] = x * n; i[0][1] = y * n; i[0][2] = z * n; // |i0| = sx / (sx * sx)
 	
 	x = m[0][1]; y = m[1][1]; z = m[2][1];
-	n = sqrtf(x * x + y * y + z * z);
-	i[1][0] = x / n; i[1][1] = y / n; i[1][2] = z / n;
+	n = 1.f / (x * x + y * y + z * z);
+	i[1][0] = x * n; i[1][1] = y * n; i[1][2] = z * n;
 	
 	x = m[0][2]; y = m[1][2]; z = m[2][2];
-	n = sqrtf(x * x + y * y + z * z);
-	i[2][0] = x / n; i[2][1] = y / n; i[2][2] = z / n;
+	n = 1.f / (x * x + y * y + z * z);
+	i[2][0] = x * n; i[2][1] = y * n; i[2][2] = z * n;
 	
 	x = m[0][3]; y = m[1][3]; z = m[2][3];
 	
